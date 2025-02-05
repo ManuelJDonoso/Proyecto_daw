503c0a524f1b357f0144652073618e5aef8adffb
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-02-05 23:30:32 +0100

    menu con metologia BEM

---
+++ includes/menu.php
@@ -0,0 +1,19 @@
+<nav class="main-nav">
+    <ul class="menu">
+        <li class="menu__item">
+            <a href="#" class="menu__link">INICIO</a>
+        </li>
+        <li class="menu__item">
+            <a href="#" class="menu__link">ACERCA DE NOSOTROS</a>
+        </li>
+        <li class="menu__item">
+            <a href="#" class="menu__link">EVENTOS</a>
+        </li>
+        <li class="menu__item">
+            <a href="#" class="menu__link">CALENDARIO</a>
+        </li>
+        <li class="menu__item">
+            <a href="#" class="menu__link">LOGIN</a>
+        </li>
+    </ul>
+</nav>
--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-    <header></header>
+    <header>
+        <?php
+        include_once 'includes/menu.php';
+        ?>
+
+    </header>
     <main></main>
     <footer></footer>
 </body>
+
 </html>
