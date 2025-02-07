41d6f46f0b85128c8f3bdda2cf498681ac1933f7
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-02-07 13:46:39 +0100

    ejemplo targeta

--- index.php
+++ index.php
@@ -15,7 +15,17 @@
         ?>
 
     </header>
-    <main></main>
+    <main>
+
+        <article class="card">
+            <img class="card__avatar" src="assets/images/acerca_de.png" alt="icono targeta">
+            <h2 class="card__title">Inicio</h2>
+            <p class="card__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam voluptate distinctio porro numquam laudantium obcaecati rem blanditiis odit, neque possimus. Obcaecati veritatis nobis accusamus sapiente reprehenderit? Minima nemo dolor sequi?</p>
+
+        </article>
+
+
+    </main>
     <footer></footer>
 </body>
 
