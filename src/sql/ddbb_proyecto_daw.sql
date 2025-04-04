2b840faae4c5f507711e3c5ca06903ccde3088b4
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-04-04 11:43:48 +0200

    modificacion del tiempo de validacion del token, modificacion del script de creaccion de la base de datos

--- src/sql/ddbb_proyecto_daw.sql
+++ src/sql/ddbb_proyecto_daw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-04-2025 a las 10:28:02
+-- Tiempo de generación: 04-04-2025 
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
