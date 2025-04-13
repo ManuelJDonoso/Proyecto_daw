168563c1a18922532810ae98db3321d1b12a5e1c
Manuel Jesús Donoso Pérez <dev@manueldonoso.es>
2025-04-13 19:38:27 +0200

    guarda respuesta de notificacion

--- src/www/controllers/notificacion.php
+++ src/www/controllers/notificacion.php
@@ -3,10 +3,21 @@
 $mensajes = $pdo->query("SELECT id, nombre_usuario, fecha,mensaje FROM vista_mensaje_help ORDER BY fecha DESC")->fetchAll(PDO::FETCH_ASSOC);
 $mensajes2="";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    var_dump($_POST);
+
+    $mensaje_id=$_POST["mensaje_id"];
+    $data = $pdo->query("SELECT usuario_id,mensaje FROM help where id= $mensaje_id")->fetch(PDO::FETCH_ASSOC);
+ 
+
+    $respuesta=$_POST["respuesta"];
+    $destinatario_id=$data["usuario_id"];
+    $remitente=$usuario->get_id();
+    $mensaje_original=$data["mensaje"];
     switch ($_POST["accion"]) {
         case 'Enviar':
-            # code...
+           echo' <br><br><br>';
+            var_dump($destinatario_id);var_dump($remitente);var_dump($respuesta);
+            $stmt = $pdo->prepare("INSERT INTO notificacion (dirigido_a, de, mensaje,mensaje_original) VALUES (?, ?, ?,?)");
+            $stmt->execute([$destinatario_id, $remitente, $respuesta,$mensaje_original  ]);
             break;
         case 'value':
             # code...
