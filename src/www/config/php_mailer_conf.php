491c8836299c11e19246b65659c3564af8188a1d
Manuel Jesus Donoso Pérez <dev@manueldonoso.es>
2025-05-12 11:47:22 +0200

    Create php_mailer_conf.php

---
+++ src/www/config/php_mailer_conf.php
@@ -0,0 +1,18 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require_once 'vendor/autoload.php';
+$mail = new PHPMailer(true);
+
+$mail->isSMTP();
+$mail->Host = 'smtp.dondominio.com'; // Servidor SMTP (Ejemplo: smtp.gmail.com)
+$mail->SMTPAuth = true;
+$mail->Username = 'dev@manueldonoso.es'; // Correo del remitente
+$mail->Password = ''; // Contraseña o App Password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->setFrom('dev@manueldonoso.es', 'Soporte');
+$mail->addAddress($email);
+$mail->Subject = 'Recuperar Acceso';
